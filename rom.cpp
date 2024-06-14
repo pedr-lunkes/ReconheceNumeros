39bcfe1352d5c2ca7455ddd43f9e88d32bc6f96d
Pedro <pedrolunkesvillela@usp.br>
2024-06-14 11:38:45 -0300

    Número de neuronios variáveis, compilação mais rápida

--- rom.cpp
+++ rom.cpp
@@ -4,33 +4,72 @@
 #define alturaImagem 28
 #define larguraImagem 28
 
-#define casosTestes 80
+#define casosTestes 120
+#define tamNeu 12
+
+#define desafio 1000
 
 using namespace std;
 using namespace cv;
 
+int pow(int a, int b){
+    if(b == 0){
+        return 1;
+    }
+
+    for(int i = 0;i<b;i++){
+        a = a << 1;
+    }
+    return a;
+}
+
 class Rede{
     public:
-        vector<vector<bool>> tabelas;
+        vector<vector<unsigned char>> tabelas;
         int tamanho;
+        int esquecimento;
 
         Rede(){
-            tamanho = alturaImagem*larguraImagem/3;
-            tabelas = vector<vector<bool>>(tamanho, vector<bool>(8, false));
+            tamanho = alturaImagem*larguraImagem/tamNeu;
+            tabelas = vector<vector<unsigned char>>(tamanho, vector<unsigned char>(pow(2, tamNeu), 0));
         }
 
-        void setUm(int chave, int x, int y, int z){
-            tabelas[chave][x * 4 + y * 2 + z] = true;
+        int indice(int *pesos){
+            int ind = 0;
+            for(int i = 0;i<tamNeu;i++){
+                ind += pesos[i] * pow(2, i);
+            }
+            return ind;
         }
 
-        int getRes(int chave, int x, int y, int z){
-            return tabelas[chave][x * 4 + y * 2 + z] == 1 ? 1 : -1;
+        void setUm(int chave, int *pesos){
+            tabelas[chave][indice(pesos)] += 1;
+        }
+
+        char getRes(int chave, int *pesos){
+            return tabelas[chave][indice(pesos)];
+        }
+
+        void esquecer(int chave, int *pesos){
+            tabelas[chave][indice(pesos)] = false;
         }
 };
 
+int *pixels(int coordenada, vector<bool> imagem, vector<vector<int>> chave){
+    int *pesos = (int *)malloc(tamNeu * sizeof(int));
+
+    for(int i = 0;i<tamNeu;i++){
+        pesos[i] = imagem[chave[coordenada][i]];
+    }
+
+    return pesos;
+}
+
 void treinamento(Rede *rede, vector<bool> imagem, vector<vector<int>> chave){
-    for(int i = 0;i< alturaImagem * larguraImagem / 3;i++){
-        rede->setUm(i, imagem[chave[i][0]], imagem[chave[i][1]], imagem[chave[i][2]]);
+    for(int i = 0;i< alturaImagem * larguraImagem / tamNeu;i++){
+        int *p = pixels(i, imagem, chave);
+        rede->setUm(i, p);
+        free(p);
     }
 }
 
@@ -80,24 +119,26 @@ int resultado(string arquivo, vector<Rede> redes, vector<vector<int>> chave){
     vector<bool> imagem = leImagem(arquivo);
 
     int maxVal1 = 0;
-    int maxVal2 = 0;
+    // int maxVal2 = 0;
     int max1 = 0;
-    int max2 = 0;
+    // int max2 = 0;
 
     for(int i = 0;i<10;i++){
         int res = 0;
-        for(int j = 0;j<alturaImagem*larguraImagem/3;j++){
-            res += redes[i].getRes(j, imagem[chave[j][0]], imagem[chave[j][1]], imagem[chave[j][2]]);
+        for(int j = 0;j<alturaImagem*larguraImagem/tamNeu;j++){
+            int *p = pixels(j, imagem, chave);
+            res += (int) redes[i].getRes(j, p);
+            free(p);
         }
 
         if(maxVal1 < res){
             maxVal1 = res;
             max1 = i;
         }
-        else if(maxVal2 < res){
-            maxVal2 = res;
-            max2 = i;
-        }
+    //     else if(maxVal2 < res){
+    //         maxVal2 = res;
+    //         max2 = i;
+    //     }
     }
 
     // cout << "Resultado mais provavel: " << max1 << endl;
@@ -111,7 +152,7 @@ int main(){
     random_device rd;
     mt19937 gen(rd());
 
-    vector<vector<int>> chave(alturaImagem*larguraImagem/3, vector<int>(3));
+    vector<vector<int>> chave(alturaImagem*larguraImagem/tamNeu, vector<int>(tamNeu));
 
     // Indices da matriz imagem
     vector<int> indices(alturaImagem*larguraImagem);
@@ -123,13 +164,12 @@ int main(){
     shuffle(indices.begin(), indices.end(), gen);
 
     // Inicialização das chaves
-    for(int i = 0;i<alturaImagem*larguraImagem/3;i++){
-        chave[i][0] = indices[i*3];
-        chave[i][1] = indices[i*3 + 1];
-        chave[i][2] = indices[i*3 + 2];
+    for(int i = 0;i<alturaImagem*larguraImagem/tamNeu;i++){
+        for(int j = 0;j<tamNeu;j++){
+            chave[i][j] = indices[i*tamNeu + j];
+        }
     }
 
-    
     // - Treinamento -
     // Rede bananas;
     // treinamento(&bananas, matriz_binaria, chave);
@@ -149,14 +189,16 @@ int main(){
         redes.push_back(Rede());
     }
 
+    uniform_int_distribution<int> distrib(0, 10000);
     for(int i = 0;i<10;i++){
         for(int j = 0;j<casosTestes;j++){
-            string nome = "dataset/" + to_string(i) + "/" + to_string(i) + "/" + to_string(j) + ".png";
+            int ran = distrib(gen);
+            string nome = "dataset/" + to_string(i) + "/" + to_string(i) + "/" + to_string(ran) + ".png";
             treinamento(&redes[i], leImagem(nome), chave);
         }
     }
 
-    // - Resultado -
+    //- Resultado -
     // while(true){
     //     string nome;
     //     cout << "Digite o nome da imagem: ";
@@ -164,21 +206,25 @@ int main(){
 
     //     nome = "dataset/" + nome + ".png";
 
-    //     if(nome == "sair.png"){
+    //     if(nome == "dataset/sair.png"){
     //         break;
     //     }
 
     //     resultado(nome, redes, chave);
     // }
+
+    // cout << resultado("dataset/1/1/100.png", redes, chave);
+
     for(int i = 0;i<10;i++){
         int acertos = 0;
-        for(int j = 100;j<200;j++){
-            string nome = "dataset/" + to_string(i) + "/" + to_string(i) + "/" + to_string(j) + ".png";
+        for(int j = casosTestes;j<casosTestes + desafio;j++){
+            int ran = distrib(gen);
+            string nome = "dataset/" + to_string(i) + "/" + to_string(i) + "/" + to_string(ran) + ".png";
             if(resultado(nome, redes, chave) == i){
                 acertos++;
             }
         }
-        acertos = acertos * 100 / 100;
+        acertos = acertos * 100 / desafio;
 
         cout << "Acertos da rede " << i << ": " << acertos << "%" << endl;
     }
